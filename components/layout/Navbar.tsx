53505c7dfb7b177497797a46a794cab1aea99ad7
ZeyarPaing <zeyarpaing@ucsy.edu.mm>
2022-05-22 20:29:24 +0630

    remove: nav

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -7,89 +7,89 @@ const Navbar = () => {
 
   return (
     <nav className="z-10 px-3 py-3 text-white bg-gray-900 bg-opacity-10 backdrop-blur-md fixed top-0 w-full">
-      <div className="m-container flex flex-wrap justify-between items-center ">
+      <div className="m-container flex flex-wrap justify-center items-center ">
         <Link href="/">
           <a>
             <Image src="/logo-full.svg" width={125} height={30} alt="logo" />
           </a>
         </Link>
-        <div
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 md:hidden "
-          aria-controls="mobile-menu"
-          aria-expanded="false"
-          onClick={() => setExpanded(!isExpanded)}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            width="27"
-            height="16"
-            viewBox="0 0 27 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              className={`transition-transform ease-linear duration-75 ${
-                isExpanded
-                  ? ' rotate-45 -translate-y-4 translate-x-3'
-                  : 'rotate-0'
-              }`}
-              d="M0.560976 14H23L22.439 16H0L0.560976 14Z"
-              fill="#D9D9D9"
-            />
-            <path
-              className={`${
-                isExpanded ? 'opacity-0' : 'opacity-100'
-              } transition-opacity duration-75`}
-              d="M7.43902 7H25L24.561 9H7L7.43902 7Z"
-              fill="#D9D9D9"
-            />
-            <path
-              className={`transition-transform ease-linear duration-75  ${
-                isExpanded ? '-rotate-45 translate-y-[18px]' : 'rotate-0'
-              } `}
-              d="M5.53659 0H27L26.4634 2H5L5.53659 0Z"
-              fill="#D9D9D9"
-            />
-          </svg>
-        </div>
-        <div
-          className={`${
-            isExpanded ? 'block' : 'hidden'
-          } w-full md:block md:w-auto`}
-        >
-          <ul className="flex flex-col mt-4 text-center md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium gap-y-3">
-            <li>
-              <Link href={'/'}>
-                <a
-                  className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent "
-                  aria-current="page"
-                >
-                  About Me
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href={'/projects'}>
-                <a
-                  className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent "
-                  aria-current="page"
-                >
-                  Projects
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href={'/blogs'}>
-                <a
-                  className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent "
-                  aria-current="page"
-                >
-                  Blogs
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </div>
+        {/*<div*/}
+        {/*  className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 md:hidden "*/}
+        {/*  aria-controls="mobile-menu"*/}
+        {/*  aria-expanded="false"*/}
+        {/*  onClick={() => setExpanded(!isExpanded)}*/}
+        {/*>*/}
+        {/*  <span className="sr-only">Open main menu</span>*/}
+        {/*  <svg*/}
+        {/*    width="27"*/}
+        {/*    height="16"*/}
+        {/*    viewBox="0 0 27 16"*/}
+        {/*    fill="none"*/}
+        {/*    xmlns="http://www.w3.org/2000/svg"*/}
+        {/*  >*/}
+        {/*    <path*/}
+        {/*      className={`transition-transform ease-linear duration-75 ${*/}
+        {/*        isExpanded*/}
+        {/*          ? ' rotate-45 -translate-y-4 translate-x-3'*/}
+        {/*          : 'rotate-0'*/}
+        {/*      }`}*/}
+        {/*      d="M0.560976 14H23L22.439 16H0L0.560976 14Z"*/}
+        {/*      fill="#D9D9D9"*/}
+        {/*    />*/}
+        {/*    <path*/}
+        {/*      className={`${*/}
+        {/*        isExpanded ? 'opacity-0' : 'opacity-100'*/}
+        {/*      } transition-opacity duration-75`}*/}
+        {/*      d="M7.43902 7H25L24.561 9H7L7.43902 7Z"*/}
+        {/*      fill="#D9D9D9"*/}
+        {/*    />*/}
+        {/*    <path*/}
+        {/*      className={`transition-transform ease-linear duration-75  ${*/}
+        {/*        isExpanded ? '-rotate-45 translate-y-[18px]' : 'rotate-0'*/}
+        {/*      } `}*/}
+        {/*      d="M5.53659 0H27L26.4634 2H5L5.53659 0Z"*/}
+        {/*      fill="#D9D9D9"*/}
+        {/*    />*/}
+        {/*  </svg>*/}
+        {/*</div>*/}
+        {/*<div*/}
+        {/*  className={`${*/}
+        {/*    isExpanded ? 'block' : 'hidden'*/}
+        {/*  } w-full md:block md:w-auto`}*/}
+        {/*>*/}
+        {/*  <ul className="flex flex-col mt-4 text-center md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium gap-y-3">*/}
+        {/*    <li>*/}
+        {/*      <Link href={'/'}>*/}
+        {/*        <a*/}
+        {/*          className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent "*/}
+        {/*          aria-current="page"*/}
+        {/*        >*/}
+        {/*          About Me*/}
+        {/*        </a>*/}
+        {/*      </Link>*/}
+        {/*    </li>*/}
+        {/*    <li>*/}
+        {/*      <Link href={'/projects'}>*/}
+        {/*        <a*/}
+        {/*          className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent "*/}
+        {/*          aria-current="page"*/}
+        {/*        >*/}
+        {/*          Projects*/}
+        {/*        </a>*/}
+        {/*      </Link>*/}
+        {/*    </li>*/}
+        {/*    <li>*/}
+        {/*      <Link href={'/blogs'}>*/}
+        {/*        <a*/}
+        {/*          className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent "*/}
+        {/*          aria-current="page"*/}
+        {/*        >*/}
+        {/*          Blogs*/}
+        {/*        </a>*/}
+        {/*      </Link>*/}
+        {/*    </li>*/}
+        {/*  </ul>*/}
+        {/*</div>*/}
       </div>
     </nav>
   );
--- components/project/ProjectCard.tsx
+++ components/project/ProjectCard.tsx
@@ -4,11 +4,14 @@ import Image from 'next/image';
 const projectCard = (props: { project: Project; showDots?: boolean }) => {
   const { project, showDots } = props;
   return (
-    <div className="relative">
+    <div
+      className="relative cursor-pointer group"
+      onClick={() => window.open(project.demoUrl, '_blank')}
+    >
       <div
         className={`${
           showDots ? 'absolute' : ''
-        } bg-gray-800 bg-opacity-40 rounded-2xl px-7 py-6 z-[3] backdrop-blur-md`}
+        } bg-gray-800 bg-opacity-40 transition-all group-hover:bg-opacity-50 rounded-2xl px-7 py-6 z-[3] backdrop-blur-md`}
       >
         <div className="flex gap-4 flex-wrap">
           <Image src={project.logo} width={50} height={50} alt={project.name} />
@@ -21,7 +24,7 @@ const projectCard = (props: { project: Project; showDots?: boolean }) => {
       </div>
       {showDots ? (
         <div
-          className="absolute -top-1.5 -left-1.5 rounded-full w-8 h-8 "
+          className="absolute -top-1.5 -left-1.5 rounded-full w-8 h-8 group-hover:-top-3 group-hover:-left-3 transition-all "
           style={{ backgroundColor: project.color }}
         ></div>
       ) : (
