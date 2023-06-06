31c667b377e0c6ce37c65389089b6b964a596468
ZeyarPaing <zeyarpaing@protonmail.com>
2023-06-06 23:06:13 +0630

    feat: added security headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compress: true,
+  poweredByHeader: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'X-Frame-Options',
+          value: 'DENY',
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'strict-origin-when-cross-origin',
+        },
+        {
+          key: 'Strict-Transport-Security',
+          value: 'max-age=31536000; includeSubDomains; preload',
+        },
+        {
+          key: 'Content-Security-Policy',
+          value: `default-src 'self';script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google-analytics.com;style-src 'self' 'unsafe-inline' *.googleapis.com;img-src * blob: data:;media-src 'none';connect-src *;font-src 'self' *.gstatic.com;`,
+        },
+      ],
+    },
+  ],
   images: {
     remotePatterns: [
       {
